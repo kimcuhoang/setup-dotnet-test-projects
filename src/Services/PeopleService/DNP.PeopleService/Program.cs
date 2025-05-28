c51e3f3d107eac56d52e19e2a35789e9d2d35ca3
Kim-CH <kim.cuhoang@gmail.com>
2025-05-28 23:22:28 +0700

    Add HealthCheck

--- src/Services/PeopleService/DNP.PeopleService/Program.cs
+++ src/Services/PeopleService/DNP.PeopleService/Program.cs
@@ -1,5 +1,7 @@
 using DNP.PeopleService.Features.People;
+using DNP.PeopleService.HealthChecks;
 using DNP.PeopleService.Persistence;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using System.Text.Encodings.Web;
 using System.Text.Unicode;
 
@@ -26,12 +28,21 @@ builder.WebHost.ConfigureKestrel(options =>
     options.Configure(builder.Configuration.GetSection("Kestrel"));
 });
 
+builder.Services
+    .AddHealthChecks()
+    .AddCustomHealthChecks();
+
 var app = builder.Build();
 
 app.MapControllers();
 
 // Configure the HTTP request pipeline.
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.MapHealthChecks("/healthz", new HealthCheckOptions
+{
+    ResponseWriter = HealthCheckRegistration.WriteResponse,
+});
 
 await app.RunAsync();
 
